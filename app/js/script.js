6e629dc33bc3d865091c8ece6abcc94c23ead197
sasha zmts <zloy.root@gmail.com>
2016-07-18 18:25:53 +0300

    init boilerplate

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+$("#color").dropkick({
+    mobile: true,
+});
+
+$('.flexslider').flexslider({
+   animation: "slide",
+   controlNav: "thumbnails",
+   directionNav: false
+ });
---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp            =           require('gulp'),
+    sass            =           require('gulp-sass'),
+    bs              =           require('browser-sync'),
+    autoprefixer    =           require('gulp-autoprefixer');
+
+gulp.task('make-sass', function() {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(autoprefixer({
+			browsers: ['last 20 versions'],
+			cascade: false
+		}))
+        .pipe(gulp.dest('app/css'))
+        .pipe(bs.reload({stream: true}))
+});
+
+gulp.task('browser-reload', function() {
+    bs({
+        server:{
+            baseDir: 'app'
+        },
+        notify: false
+    })
+})
+
+gulp.task('watch', ['browser-reload', 'make-sass'], function() {
+    gulp.watch('src/scss/**/*.scss', ['make-sass']);
+    gulp.watch('src/scss/**/*.css', ['make-sass']);
+    gulp.watch('app/*.html', bs.reload);
+    // gulp.watch('app/js/*.js', bs.reload);
+});
+
+gulp.task('default', ['watch']);
